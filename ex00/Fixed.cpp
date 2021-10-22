c34dc421a782422f0e623385a25788d7356163dd
Carmine Watcher <cwatcher@student.21-school.r>
2021-10-22 17:00:59 +0300

    + messages

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -2,13 +2,21 @@
 #include <iostream>
 
 Fixed::Fixed( void ): _rawBits( 0 )
-{}
-Fixed::Fixed( const Fixed& x ): _rawBits( x.getRawBits() )
-{}
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+Fixed::Fixed( const Fixed& x )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	_rawBits = x.getRawBits();
+}
 Fixed::~Fixed( void )
-{}
+{
+	std::cout << "Destructor called" << std::endl;
+}
 int Fixed::getRawBits() const
 {
+	std::cout << "getRawBits member function called" << std::endl;
 	return _rawBits;
 }
 void Fixed::setRawBits( int x )

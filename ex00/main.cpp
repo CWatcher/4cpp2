5d484f79b529dbb1c03ab807127f1097fd86a44f
Carmine Watcher <cwatcher@student.21-school.r>
2021-10-22 17:02:31 +0300

    + assignment_test()

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,8 +1,22 @@
 #include "Fixed.hpp"
 #include <iostream>
 
+void null_int(int& x)
+{
+	x = 0;
+}
+void assignment_test()
+{
+	int a = 1;
+	int b = 10;
+	int c = 100;
+	std::cout << (a=b=c) << std::endl;
+	null_int(a=c);
+	std::cout << a << std::endl;
+}
 int main( void )
 {
+	assignment_test();
 	Fixed a;
 	Fixed b( a );
 	Fixed c;

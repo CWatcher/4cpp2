6455975fc95ce6e10fa2cf4b6837fe60f31c221f
Carmine Watcher <CWatcher@student.21-school.r>
2021-11-01 16:00:07 +0300

    [formatting] ex01, ex02 t.sh += alignment

--- ex01/t.sh
+++ ex01/t.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-make > /dev/null		\
-	&& echo make ok! 	\
+make > /dev/null							\
+	&& printf "%-27s: %s\n" 'make' 'ok!' 	\
 	|| exit 1
 
-valgrind -q aa | diff subject.output.txt - && echo ok!
+valgrind -q aa | diff subject.output.txt -	\
+	&& echo Compare with subject output: ok!
+printf "\n%-s:\n\n" "Extended test"
 aa --extended

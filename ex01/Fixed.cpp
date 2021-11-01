5956b49f536245d91e57a885b6753ed0d9e8991c
Carmine Watcher <CWatcher@student.21-school.r>
2021-11-01 15:49:32 +0300

    [formatting] ex01/Fixed.cpp -= 7 "{" lines

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,8 +6,7 @@ const int	Fixed::_nFractionalBits = 8;
 const int	Fixed::_nFractionalCapacity = 1 << _nFractionalBits;
 
 Fixed::Fixed( void ): _rawBits( 0 )
-{
-	std::cout << "Default constructor called" << std::endl;
+{	std::cout << "Default constructor called" << std::endl;
 }
 Fixed::Fixed( const Fixed& x )
 {
@@ -25,17 +24,14 @@ Fixed::	Fixed( const float x )
 	_rawBits = roundf(x * _nFractionalCapacity);
 }
 Fixed::~Fixed( void )
-{
-	std::cout << "Destructor called" << std::endl;
+{	std::cout << "Destructor called" << std::endl;
 }
 
 int		Fixed::getRawBits( void ) const
-{
-	return _rawBits;
+{	return _rawBits;
 }
 void 	Fixed::setRawBits( int x )
-{
-	_rawBits = x;
+{	_rawBits = x;
 }
 Fixed& 	Fixed::operator=( const Fixed& x )
 {
@@ -44,20 +40,16 @@ Fixed& 	Fixed::operator=( const Fixed& x )
 	return *this;
 }
 int 	Fixed::toInt( void ) const
-{
-	return _rawBits >> _nFractionalBits;
+{	return _rawBits >> _nFractionalBits;
 }
 double 	Fixed::toDouble( void ) const
-{
-	return ( double )_rawBits / _nFractionalCapacity;
+{	return ( double )_rawBits / _nFractionalCapacity;
 }
 float 	Fixed::toFloat( void ) const
-{
-	return ( float )_rawBits / _nFractionalCapacity;
+{	return ( float )_rawBits / _nFractionalCapacity;
 }
 std::ostream&	operator<<( std::ostream& os, const Fixed& x )
 {
-//	os << x.toFloat();
 	os << x.toDouble();
 	return os;
 }

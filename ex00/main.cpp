3f5644ae287eafcf52e522565d1e42c802bb1211
Carmine Watcher <CWatcher@student.21-school.r>
2021-11-01 12:47:40 +0300

    ex00 += extended test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,8 +1,29 @@
 #include "Fixed.hpp"
 #include <iostream>
+#include <climits>
 
-int main( void )
+void	showSetGet(int i)
 {
+	static Fixed f;
+
+	f.setRawBits( i );
+	std::cout << "setRawBits(     " << i << " )" << std::endl;
+	i = f.getRawBits();
+	std::cout << "getRawBits() == " << i << std::endl;
+	std::cout << std::endl;
+}
+void	test()
+{
+	showSetGet( -1234567890 );
+	showSetGet( INT_MAX );
+	showSetGet( INT_MIN );
+} 
+int 	main( int argc, char** )
+{
+	if ( argc > 1 )
+	{	test();
+		return 0;
+	}
 	Fixed a;
 	Fixed b( a );
 	Fixed c;
